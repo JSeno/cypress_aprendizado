85878aaf8d6baa061df62c81d5ce4008fd8e03b9
Jefferson Seno <jeffersonseno@datacake.com.br>
2022-07-13 01:05:31 -0300

    Teste de clicar em botão, métodos .debug() e .pause()

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,21 +1,32 @@
 /// <reference types="Cypress" />
 
 describe('Cypress basics', () => {
-   it('Should visit a page and assert title', () => {
-     cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+   it.only('Should visit a page and assert title', () => {
+      cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
-     // const title = cy.title()
-     // console.log(title)
+   // const title = cy.title()
+   // console.log(title)
 
-     cy.title().should('be.equal', 'Campo de Treinamento')
-     cy.title().should('contain', 'Campo')
+      // o .pause() pausa a execução do teste por um determinado tempo
+      cy.pause()
 
-     cy.title()
-        .should('be.equal', 'Campo de Treinamento')
-        .and('contain', 'Campo')
+      cy.title().should('be.equal', 'Campo de Treinamento')
+      // o .debug para ver no console o que está sendo retornado, posso colocar ele em qualquer lugar
+      cy.title().should('contain', 'Campo').debug()
 
-    // TODO: Imprimir o log no console.
-    //TODO: Escrever o nome no campo de texto.
+      cy.title()
+      .should('be.equal', 'Campo de Treinamento')
+      .and('contain', 'Campo')
 
-   });
-});
+   // TODO: Imprimir o log no console.
+   //TODO: Escrever o nome no campo de texto.
+   })
+
+   it('Should find and interact with an element', () => {
+      cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+      cy.get('#buttonSimple').click()
+      // Criar uma assertiva para confirmar o teste. da automação
+      cy.get('#buttonSimple').should('have.value', 'Obrigado!')
+
+   })
+   })
