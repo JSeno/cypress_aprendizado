9ed88e6359d7b85c3ee42255175aa58fc52dd969
Jefferson Seno <jeffersonseno@datacake.com.br>
2022-07-13 08:29:13 -0300

    Adicionado aula de busca de um texto dentro da página.

---
+++ cypress/integration/element.spec.js
@@ -0,0 +1,20 @@
+// reference types="Cypress" eu estou dizendo que estou usando o Cypress.
+
+/// <reference types="cypress" /> 
+
+// describe é um grupo de testes, ou seja, um conjunto de testes.
+describe('Word with basic elements', () => {
+    // o it é um teste, ou seja, um caso de teste.
+    it('Text', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        // o get('Aqui coloco uma busca') o should é para verificar se o elemento existe.
+        // Essa função está buscando no corpo se existe uma palavra com o texto 'Cuidado'.
+        cy.get('body').should('contain', 'Cuidado')
+        // cy.get('body').should('have.text', 'Cuidado')
+        cy.get('span').should('contain', 'Cuidado')
+        // cy.get('div').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') // Aqui é esperado o texto exato.
+    })
+})
